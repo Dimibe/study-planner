950ef25efad0287cab506261ca0a8d929b3462f2
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-08 17:41:01 +0200

    Add dialogs to app drawer

--- lib/widgets/SPDrawer.dart
+++ lib/widgets/SPDrawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:study_planner/pages/GeneralInformation.page.dart';
+import 'package:study_planner/pages/SemesterOverview.page.dart';
 import 'package:study_planner/pages/Settings.page.dart';
 
 class SPDrawer extends StatelessWidget {
@@ -20,8 +22,27 @@ class SPDrawer extends StatelessWidget {
             ),
           ),
           ListTile(
+            leading: Icon(Icons.featured_play_list),
+            title: Text('Allgemeine Informationen'),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                    builder: (context) => GeneralInformationPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.list_alt),
+            title: Text('Semester Übersicht'),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => SemesterOverviewPage()),
+              );
+            },
+          ),
+          ListTile(
             leading: Icon(Icons.settings),
-            title: Text('Settings'),
+            title: Text('Einstellungen'),
             onTap: () {
               Navigator.of(context).pushReplacement(
                 MaterialPageRoute(builder: (context) => SettingsPage()),
