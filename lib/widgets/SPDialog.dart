499039449fb636ed96bbf18eb1afcd173c227e59
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-08 23:26:35 +0200

    Change app draw pop up text

--- lib/widgets/SPDialog.dart
+++ lib/widgets/SPDialog.dart
@@ -5,13 +5,24 @@ class SPDialog extends StatelessWidget {
   final String title;
   final dynamic content;
 
-  SPDialog({@required this.title, @required this.content});
+  SPDialog({@required this.content, this.title = 'Study Planner!'});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(this.title),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: 'Menu',
+            );
+          },
+        ),
       ),
       drawerScrimColor: Theme.of(context).backgroundColor,
       drawer: SPDrawer(),
