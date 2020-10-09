2d031aea46cd2b06bc5f9d64c7a4934e6c6cacae
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-09 19:56:26 +0200

    Refactor analysis page

--- lib/pages/AnalysisOverview.page.dart
+++ lib/pages/AnalysisOverview.page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:study_planner/models/Course.dart';
 import 'package:study_planner/models/Semester.dart';
-import 'package:study_planner/models/StudyPlan.dart';
 import 'package:study_planner/services/StorageService.dart';
+import 'package:study_planner/widgets/SPBarChart.dart';
 import 'package:study_planner/widgets/SPDialog.dart';
-import 'package:charts_flutter/flutter.dart';
 
 class AnalysisOverviewPage extends StatefulWidget {
   @override
@@ -12,97 +10,38 @@ class AnalysisOverviewPage extends StatefulWidget {
 }
 
 class _AnalysisOverviewPageState extends State<AnalysisOverviewPage> {
-  StudyPlan _studyPlan;
-  List<Series<dynamic, String>> _creditsData = [];
-  List<Series<dynamic, String>> _gradesData = [];
-
-  @override
-  void initState() {
-    super.initState();
-    StorageService.loadStudyPlan().then(
-      (p) => setState(() {
-        _studyPlan = p;
-        _creditsData = _getCreditsData();
-        _gradesData = _getGradesData();
-      }),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return SPDialog(
-      content: [
-        Container(
-          padding: EdgeInsets.all(8.0),
-          width: 300,
-          height: 300,
-          child: BarChart(
-            _creditsData,
-            animate: true,
-            defaultInteractions: false,
-            behaviors: [ChartTitle('Credits')],
-            barRendererDecorator: BarLabelDecorator<String>(),
+      dependsOn: StorageService.loadStudyPlan(),
+      content: (studyPlan) {
+        return <Widget>[
+          SPBarChart<Semester>(
+            id: 'credits',
+            title: 'Credits',
+            data: studyPlan.semester,
+            domainFn: (s, _) => s.name,
+            measureFn: (s, _) =>
+                s.courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(8.0),
-          width: 300,
-          height: 300,
-          child: BarChart(
-            _gradesData,
-            animate: true,
-            defaultInteractions: false,
-            behaviors: [ChartTitle('Notendurchschnitt')],
-            barRendererDecorator: BarLabelDecorator<String>(),
+          SPBarChart(
+            id: 'courses',
+            title: 'Notendurchschnitt',
+            data: studyPlan.semester,
+            domainFn: (s, _) => s.name,
+            measureFn: (s, _) {
+              var courses = s.courses.where((c) => c.grade != null);
+              var sum = courses
+                  .map((c) => c.grade * c.credits)
+                  .reduce((v1, v2) => v1 + v2);
+
+              var mean = (sum /
+                  courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2));
+              return num.parse(mean.toStringAsFixed(2));
+            },
           ),
-        ),
-      ],
+        ];
+      },
     );
-  }
-
-  List<Series<Semester, String>> _getGradesData() {
-    num _measure(semester, _) {
-      var courses = semester.courses.where((c) => c.grade != null);
-      var sum =
-          courses.map((c) => c.grade * c.credits).reduce((c1, c2) => c1 + c2);
-
-      var mean =
-          (sum / courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2));
-      return num.parse(mean.toStringAsFixed(2));
-    }
-
-    return [
-      Series(
-        id: 'courses',
-        data: _studyPlan.semester,
-        domainFn: (semester, _) => semester.name,
-        measureFn: _measure,
-        labelAccessorFn: (semester, _) => '${_measure(semester, _)}',
-        colorFn: (semester, _) {
-          var theme = Theme.of(context).accentColor;
-          return Color(r: theme.red, g: theme.green, b: theme.blue);
-        },
-      ),
-    ];
-  }
-
-  List<Series<Semester, String>> _getCreditsData() {
-    num _measure(semester, _) {
-      return semester.courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2);
-    }
-
-    return [
-      Series(
-        id: 'courses',
-        data: _studyPlan.semester,
-        domainFn: (semester, _) => semester.name,
-        measureFn: _measure,
-        labelAccessorFn: (semester, _) => '${_measure(semester, _)}',
-        colorFn: (semester, _) {
-          var theme = Theme.of(context).accentColor;
-          return Color(r: theme.red, g: theme.green, b: theme.blue);
-        },
-      ),
-    ];
   }
 }
