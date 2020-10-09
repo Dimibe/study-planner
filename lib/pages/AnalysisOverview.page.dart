42740593d0fa7e4e1faeeb6d6c4f4303672eb9fc
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-09 12:14:48 +0200

    Added mean grade chart to analysis page

--- lib/pages/AnalysisOverview.page.dart
+++ lib/pages/AnalysisOverview.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_planner/models/Course.dart';
 import 'package:study_planner/models/Semester.dart';
 import 'package:study_planner/models/StudyPlan.dart';
 import 'package:study_planner/services/StorageService.dart';
@@ -12,7 +13,8 @@ class AnalysisOverviewPage extends StatefulWidget {
 
 class _AnalysisOverviewPageState extends State<AnalysisOverviewPage> {
   StudyPlan _studyPlan;
-  List<Series<dynamic, String>> data = [];
+  List<Series<dynamic, String>> _creditsData = [];
+  List<Series<dynamic, String>> _gradesData = [];
 
   @override
   void initState() {
@@ -20,7 +22,8 @@ class _AnalysisOverviewPageState extends State<AnalysisOverviewPage> {
     StorageService.loadStudyPlan().then(
       (p) => setState(() {
         _studyPlan = p;
-        data = getData();
+        _creditsData = _getCreditsData();
+        _gradesData = _getGradesData();
       }),
     );
   }
@@ -30,26 +33,71 @@ class _AnalysisOverviewPageState extends State<AnalysisOverviewPage> {
     return SPDialog(
       content: [
         Container(
-          width: 400,
-          height: 400,
+          padding: EdgeInsets.all(8.0),
+          width: 300,
+          height: 300,
           child: BarChart(
-            data,
+            _creditsData,
             animate: true,
-            // barGroupingType: BarGroupingType.stacked,
+            defaultInteractions: false,
+            behaviors: [ChartTitle('Credits')],
+            barRendererDecorator: BarLabelDecorator<String>(),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(8.0),
+          width: 300,
+          height: 300,
+          child: BarChart(
+            _gradesData,
+            animate: true,
+            defaultInteractions: false,
+            behaviors: [ChartTitle('Notendurchschnitt')],
+            barRendererDecorator: BarLabelDecorator<String>(),
           ),
         ),
       ],
     );
   }
 
-  List<Series<Semester, String>> getData() {
+  List<Series<Semester, String>> _getGradesData() {
+    num _measure(semester, _) {
+      var courses = semester.courses.where((c) => c.grade != null);
+      var sum =
+          courses.map((c) => c.grade * c.credits).reduce((c1, c2) => c1 + c2);
+
+      var mean =
+          (sum / courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2));
+      return num.parse(mean.toStringAsFixed(2));
+    }
+
     return [
       Series(
         id: 'courses',
         data: _studyPlan.semester,
         domainFn: (semester, _) => semester.name,
-        measureFn: (semester, _) =>
-            semester.courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2),
+        measureFn: _measure,
+        labelAccessorFn: (semester, _) => '${_measure(semester, _)}',
+        colorFn: (semester, _) {
+          var theme = Theme.of(context).accentColor;
+          return Color(r: theme.red, g: theme.green, b: theme.blue);
+        },
+      ),
+    ];
+  }
+
+  List<Series<Semester, String>> _getCreditsData() {
+    num _measure(semester, _) {
+      return semester.courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2);
+    }
+
+    return [
+      Series(
+        id: 'courses',
+        data: _studyPlan.semester,
+        domainFn: (semester, _) => semester.name,
+        measureFn: _measure,
+        labelAccessorFn: (semester, _) => '${_measure(semester, _)}',
         colorFn: (semester, _) {
           var theme = Theme.of(context).accentColor;
           return Color(r: theme.red, g: theme.green, b: theme.blue);
