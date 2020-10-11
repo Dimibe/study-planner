0024fd9196c23d1abca62e1348bd9f90c9e8d684
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-11 17:38:29 +0200

    Refactored code: use modal dialog widget

--- lib/pages/SemesterDetail.page.dart
+++ lib/pages/SemesterDetail.page.dart
@@ -3,6 +3,7 @@ import 'package:study_planner/models/Course.dart';
 import 'package:study_planner/models/Semester.dart';
 import 'package:study_planner/models/StudyPlan.dart';
 import 'package:study_planner/services/StorageService.dart';
+import 'package:study_planner/widgets/SPModalDialog.dart';
 import 'package:study_planner/widgets/common/CWButton.dart';
 import 'package:study_planner/widgets/common/CWDynamicContainer.dart';
 import 'package:study_planner/widgets/common/CWTextField.dart';
@@ -29,104 +30,92 @@ class _SemesterDetailPageState extends State<SemesterDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      ),
-      child: Container(
-        padding: EdgeInsets.all(30.0),
-        constraints: BoxConstraints(maxWidth: 700),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CWTextField(
-                labelText: 'Semester',
-                controller: myController0,
-              ),
-              CWDynamicContainer(
-                showAddOption: true,
-                showHideOption: true,
-                padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-                contoller: dynamicControllers,
-                initialData: () {
-                  return widget.semester?.courses
-                      ?.map((c) => {
-                            'Kurs Name': c.name,
-                            'Anzahl Credits': c.credits,
-                            'Note': c.grade
-                          })
-                      ?.toList(growable: false);
-                },
-                children: [
-                  CWTextField(
-                    labelText: 'Kurs Name',
-                    maxWidth: 200,
-                    //   validate: (text) => text.isNotEmpty,
-                  ),
-                  CWTextField(
-                    labelText: 'Anzahl Credits',
-                    maxWidth: 200,
-                  ),
-                  CWTextField(
-                    labelText: 'Note',
-                    maxWidth: 200,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  FlatButton(
-                    padding: EdgeInsets.only(right: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    ),
-                    onPressed: widget.semester == null
-                        ? null
-                        : () {
-                            widget.plan.semester.remove(widget.semester);
-                            StorageService.saveStudyPlan(widget.plan);
-                            Navigator.pop(context, true);
-                          },
-                    child: Text(
-                      'Löschen',
-                      style: TextStyle(color: Theme.of(context).errorColor),
-                    ),
-                  ),
-                  CWButton(
-                    label: 'Speichern',
-                    onPressed: () {
-                      var courses = <Course>[];
-                      for (Map<String, TextEditingController> c
-                          in dynamicControllers.controllers) {
-                        var course = Course(
-                          c['Kurs Name'].text,
-                          int.parse(c['Anzahl Credits'].text),
-                        );
-                        if (c['Note'].text != null &&
-                            c['Note'].text.isNotEmpty) {
-                          course.grade = double.parse(c['Note'].text);
-                        }
-                        courses.add(course);
-                      }
-                      var s = widget.semester ?? Semester();
-                      s.name = myController0.text;
-                      s.courses = courses;
-                      if (widget.semester == null) {
-                        widget.plan.semester.add(s);
-                      }
-                      StorageService.saveStudyPlan(widget.plan);
-                      Navigator.pop(context, true);
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
+    return SPModalDialog(
+      content: [
+        CWTextField(
+          labelText: 'Semester',
+          controller: myController0,
         ),
-      ),
+        CWDynamicContainer(
+          showAddOption: true,
+          showHideOption: true,
+          padding: EdgeInsets.only(top: 8.0),
+          contoller: dynamicControllers,
+          initialData: () {
+            return widget.semester?.courses
+                ?.map((c) => {
+                      'Kurs Name': c.name,
+                      'Anzahl Credits': c.credits,
+                      'Note': c.grade
+                    })
+                ?.toList(growable: false);
+          },
+          children: [
+            CWTextField(
+              labelText: 'Kurs Name',
+              maxWidth: 200,
+            ),
+            CWTextField(
+              labelText: 'Anzahl Credits',
+              maxWidth: 200,
+            ),
+            CWTextField(
+              labelText: 'Note',
+              maxWidth: 200,
+            ),
+          ],
+        ),
+      ],
+      actions: (constraints) {
+        var minWidth = 290.0;
+        if (constraints.maxWidth < 450) {
+          minWidth = 100.0;
+        }
+        return [
+          FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            ),
+            onPressed: widget.semester == null
+                ? null
+                : () {
+                    widget.plan.semester.remove(widget.semester);
+                    StorageService.saveStudyPlan(widget.plan);
+                    Navigator.pop(context, true);
+                  },
+            child: Text(
+              'Löschen',
+              style: TextStyle(color: Theme.of(context).errorColor),
+            ),
+          ),
+          CWButton(
+            label: 'Speichern',
+            minWidth: minWidth,
+            onPressed: () {
+              var courses = <Course>[];
+              for (Map<String, TextEditingController> c
+                  in dynamicControllers.controllers) {
+                var course = Course(
+                  c['Kurs Name'].text,
+                  int.parse(c['Anzahl Credits'].text),
+                );
+                if (c['Note'].text != null && c['Note'].text.isNotEmpty) {
+                  course.grade = double.parse(c['Note'].text);
+                }
+                courses.add(course);
+              }
+              var s = widget.semester ?? Semester();
+              s.name = myController0.text;
+              s.courses = courses;
+              if (widget.semester == null) {
+                widget.plan.semester.add(s);
+              }
+              StorageService.saveStudyPlan(widget.plan);
+              Navigator.pop(context, true);
+            },
+          ),
+        ];
+      },
     );
   }
 }
