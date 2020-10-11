5d95193460eba97d8359498598367eb2cb12a2be
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-11 12:11:21 +0200

    Add StudyPlan util for calculations and add maths util for convenient fuctions

---
+++ lib/utils/MathUtils.dart
@@ -0,0 +1,9 @@
+class MathUtils {
+  static T sum<T extends num>(T n1, T n2) {
+    return n1 + n2;
+  }
+
+  static T product<T extends num>(T n1, T n2) {
+    return n1 * n2;
+  }
+}
---
+++ lib/utils/StudyPlanUtils.dart
@@ -0,0 +1,28 @@
+import 'package:study_planner/models/Semester.dart';
+import 'package:study_planner/models/StudyPlan.dart';
+import 'package:study_planner/utils/MathUtils.dart';
+
+class StudyPlanUtils {
+  static int creditsInSemester(Semester semester) {
+    return semester.courses.map((c) => c.credits).reduce(MathUtils.sum);
+  }
+
+  static int sumOfCredits(StudyPlan studyPlan) {
+    return studyPlan.semester.map(creditsInSemester).reduce(MathUtils.sum);
+  }
+
+  static double meanGrade(Semester semester) {
+    var courses = semester.courses.where((c) => c.grade != null).toList();
+    if (courses.isEmpty) {
+      return 0;
+    }
+    var sum = courses.map((c) => c.grade * c.credits).reduce(MathUtils.sum);
+    var mean = sum / courses.map((c) => c.credits).reduce(MathUtils.sum);
+    return mean;
+  }
+
+  static double totalMeanGrade(StudyPlan studyPlan) {
+    return studyPlan.semester.map(meanGrade).reduce(MathUtils.sum) /
+        studyPlan.semester.length;
+  }
+}
