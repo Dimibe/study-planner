13cef8d23866e2cea4a0bf26cac43fcaf4a0957d
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-11 12:12:03 +0200

    Extend analysis overview with further information

--- lib/pages/AnalysisOverview.page.dart
+++ lib/pages/AnalysisOverview.page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:study_planner/models/Semester.dart';
+import 'package:study_planner/models/StudyPlan.dart';
 import 'package:study_planner/services/StorageService.dart';
+import 'package:study_planner/utils/StudyPlanUtils.dart';
 import 'package:study_planner/widgets/SPBarChart.dart';
 import 'package:study_planner/widgets/SPDialog.dart';
 
@@ -14,31 +16,68 @@ class _AnalysisOverviewPageState extends State<AnalysisOverviewPage> {
   Widget build(BuildContext context) {
     return SPDialog(
       dependsOn: StorageService.loadStudyPlan(),
-      content: (studyPlan) {
+      content: (StudyPlan studyPlan) {
+        var creditsNow = StudyPlanUtils.sumOfCredits(studyPlan);
+        var meanCreditsSemster = creditsNow / studyPlan.semester.length;
+        var creditsTotal = int.parse(studyPlan.creditsMain) +
+            int.parse(studyPlan.creditsOther);
+        var creditsOpen = creditsTotal - creditsNow;
+        var semesterOpen = creditsOpen / meanCreditsSemster;
+        var semesterCount = studyPlan.semester.length;
+        var openSemester = int.parse(studyPlan.semesterCount) - semesterCount;
+        var creditsPerSemester = creditsOpen / openSemester;
+
         return <Widget>[
+          SizedBox(
+            width: double.infinity,
+            child: DataTable(
+              columns: [
+                DataColumn(label: Text('Titel')),
+                DataColumn(label: Text('Wert')),
+              ],
+              rows: [
+                DataRow(
+                  cells: [
+                    DataCell(Text('Durchschnitt Credits pro Semester')),
+                    DataCell(Text('$meanCreditsSemster')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('Durchschnitt Note')),
+                    DataCell(
+                        Text('${StudyPlanUtils.totalMeanGrade(studyPlan)}')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(
+                        Text('Verbleibende Semester mit Credits Drchschnitt')),
+                    DataCell(Text('$semesterOpen')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('Credits pro Semester für Wunschziel')),
+                    DataCell(Text('$creditsPerSemester')),
+                  ],
+                )
+              ],
+            ),
+          ),
           SPBarChart<Semester>(
             id: 'credits',
             title: 'Credits',
             data: studyPlan.semester,
             domainFn: (s, _) => s.name,
-            measureFn: (s, _) =>
-                s.courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2),
+            measureFn: (s, _) => StudyPlanUtils.creditsInSemester(s),
           ),
           SPBarChart(
             id: 'courses',
             title: 'Notendurchschnitt',
             data: studyPlan.semester,
             domainFn: (s, _) => s.name,
-            measureFn: (s, _) {
-              var courses = s.courses.where((c) => c.grade != null);
-              var sum = courses
-                  .map((c) => c.grade * c.credits)
-                  .reduce((v1, v2) => v1 + v2);
-
-              var mean = (sum /
-                  courses.map((c) => c.credits).reduce((c1, c2) => c1 + c2));
-              return num.parse(mean.toStringAsFixed(2));
-            },
+            measureFn: (s, _) => StudyPlanUtils.meanGrade(s),
           ),
         ];
       },
