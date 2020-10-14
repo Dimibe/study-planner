5df981ade21c61d468b757fc7bcf62d8572589e2
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-10-14 17:06:39 +0200

    Cache studyplan to avoid long loading times

--- lib/services/StudyPlanService.dart
+++ lib/services/StudyPlanService.dart
@@ -6,23 +6,33 @@ import 'UserService.dart';
 import 'StorageService.dart';
 
 class StudyPlanService {
+  ///Cache entity to avoid long loading time.
+  StudyPlan _studyPlan;
+
   StudyPlanService();
 
-  Future<StudyPlan> loadStudyPlan() async {
+  Future<StudyPlan> loadStudyPlan({force = false}) async {
+    if (!force && _studyPlan != null) {
+      return _studyPlan;
+    }
+    StudyPlan studyPlan;
     if (GetIt.I<UserService>().isLoggedIn) {
       var uid = GetIt.I<UserService>().getUid();
       var document = GetIt.I<FirestoreService>().getDocument('studyplans', uid);
       var json = (await document).data();
       if (json == null) {
-        return StudyPlan();
+        studyPlan = StudyPlan();
       }
-      return StudyPlan.fromJson(json);
+      studyPlan = StudyPlan.fromJson(json);
     } else {
-      return GetIt.I<StorageService>().loadStudyPlan();
+      studyPlan = GetIt.I<StorageService>().loadStudyPlan();
     }
+
+    return studyPlan;
   }
 
   Future<void> saveStudyPlan(StudyPlan studyPlan) async {
+    _studyPlan = studyPlan;
     if (GetIt.I<UserService>().isLoggedIn) {
       var uid = GetIt.I<UserService>().getUid();
       return GetIt.I<FirestoreService>()
