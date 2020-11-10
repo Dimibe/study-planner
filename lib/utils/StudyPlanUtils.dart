133d5318b2a434637f5d0aa078ab9cc59d0b5804
Dimitrios Begnis <dimitribegnis@gmail.com>
2020-11-10 23:10:59 +0100

    Re-style Analysis page #44

--- lib/utils/StudyPlanUtils.dart
+++ lib/utils/StudyPlanUtils.dart
@@ -9,8 +9,11 @@ class StudyPlanUtils {
     return semester.courses.map((c) => c.credits).reduce(MathUtils.sum);
   }
 
-  static int sumOfCredits(StudyPlan studyPlan) {
-    return studyPlan.semester.map(creditsInSemester).reduce(MathUtils.sum);
+  static int sumOfCredits(StudyPlan studyPlan, {bool onlyCompleted = false}) {
+    return studyPlan.semester
+        .where((semester) => !onlyCompleted || semester.completed)
+        .map(creditsInSemester)
+        .reduce(MathUtils.sum);
   }
 
   static double semesterMeanGrade(Semester semester) {
@@ -18,9 +21,32 @@ class StudyPlanUtils {
     return _meanGrade(courses);
   }
 
-  static double totalMeanGrade(StudyPlan studyPlan) {
-    var courses = studyPlan.semester.expand(_coursesForGrading).toList();
+  static double totalMeanGrade(StudyPlan studyPlan,
+      {bool onlyCompleted = false}) {
+    var courses = studyPlan.semester
+        .where((semester) => !onlyCompleted || semester.completed)
+        .expand(_coursesForGrading)
+        .toList();
     return _meanGrade(courses);
+  }
+
+  static double bestPossibleMeanGrade(StudyPlan studyPlan,
+      {bool onlyCompleted = false}) {
+    var allCredits = studyPlan.studyFields
+        .where((field) => field.countForGrade)
+        .map((field) => field.credits)
+        .reduce(MathUtils.sum);
+
+    var takenCredits = studyPlan.semester
+        .where((semester) => !onlyCompleted || semester.completed)
+        .expand(_coursesForGrading)
+        .map((c) => c.credits)
+        .reduce(MathUtils.sum);
+
+    return (takenCredits *
+                totalMeanGrade(studyPlan, onlyCompleted: onlyCompleted) +
+            (allCredits - takenCredits)) /
+        allCredits;
   }
 
   static StudyField getStudyFieldByName(StudyPlan studyPlan, String fieldName) {
---
+++ lib/widgets/common/CWCard.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+import 'CWText.dart';
+
+class CWCard extends StatelessWidget {
+  final Color color;
+  final String title;
+  final String subTitle;
+  final String trailingTitle;
+  final EdgeInsetsGeometry margin;
+  final double width;
+  final double height;
+  final void Function() onTap;
+  final List<String> info;
+  final List<String> subInfo;
+
+  const CWCard({
+    Key key,
+    this.onTap,
+    this.title,
+    this.subTitle = '',
+    this.trailingTitle = '',
+    this.color = Colors.blue,
+    this.info = const [],
+    this.subInfo = const [],
+    this.margin = const EdgeInsets.all(8.0),
+    this.width = 300,
+    this.height = 400,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap ?? () {},
+      child: Container(
+        height: height,
+        width: width,
+        margin: margin,
+        decoration: BoxDecoration(
+          color: color.withAlpha(220),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(400.0),
+                  bottomLeft: Radius.circular(400.0),
+                  bottomRight: Radius.elliptical(2000.0, 8500.0),
+                ),
+                child: Container(
+                  width: width / 2 + 20,
+                  decoration: BoxDecoration(color: color),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 15.0,
+              left: 20.0,
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CWText(
+                      title,
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF).withOpacity(0.8),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    CWText(
+                      subTitle,
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF).withOpacity(0.8),
+                        fontStyle: FontStyle.italic,
+                        fontSize: 19.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 15.0,
+              right: 20.0,
+              child: CWText(
+                trailingTitle,
+                style: TextStyle(
+                  color: Color(0xFFFFFFFF).withOpacity(0.8),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 80.0,
+              left: 14.0,
+              right: 14.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Divider(
+                    height: 10,
+                    color: Color(0xFFFFFFFF).withOpacity(0.8),
+                  ),
+                  for (var text in info)
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: CWText(
+                        text,
+                        style: TextStyle(
+                          color: Color(0xFFFFFFFF).withOpacity(0.8),
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  for (var text in subInfo)
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 4, 6.0, 4),
+                      child: CWText(
+                        text,
+                        style: TextStyle(
+                          color: Color(0xFFFFFFFF).withOpacity(0.8),
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
